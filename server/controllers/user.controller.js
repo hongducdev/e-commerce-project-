279fd1ee5c687c05532ec6fdfcdfe8ef22a33fb8
hongduccodedao <contact.hongduc@gmail.com>
2023-06-09 16:24:28 +0700

    ⚙️ Config route Login

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,21 +2,48 @@ const User = require("../models/user.model.js");
 const asyncHandler = require("express-async-handler");
 
 const register = asyncHandler(async (req, res) => {
-  const { email, password, firstName, lastName } = req.body;
+  const { email, password, firstName, lastName, mobile } = req.body;
 
-  if (!email || !password || !firstName || !lastName) {
+  if (!email || !password || !firstName || !lastName || !mobile) {
     res.status(400).json({
       success: false,
       message: "Please fill all the fields!",
     });
   }
 
-  const response = await User.create(req.body);
-  return res.status(200).json({
-    success: response ? true : false,
-    message: response ? "User created successfully!" : "User creation failed!",
-    response,
-  });
+  const user = await User.findOne({ email });
+  if (user) {
+    throw new Error("User already exists!");
+  } else {
+    const newUser = await User.create(req.body);
+    return res.status(200).json({
+      success: newUser ? true : false,
+      message: newUser ? "User created successfully!" : "User creation failed!",
+    });
+  }
 });
 
-module.exports = { register };
+const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({
+      success: false,
+      message: "Please fill all the fields!",
+    });
+  }
+
+  const response = await User.findOne({ email });
+  if (response && (await response.isCorrectPassword(password))) {
+    const { password, role, ...userData } = response.toObject();
+    return res.status(200).json({
+      success: true,
+      message: "User logged in successfully!",
+      userData,
+    });
+  } else {
+    throw new Error("Invalid email or password!");
+  }
+});
+
+module.exports = { register, login };
